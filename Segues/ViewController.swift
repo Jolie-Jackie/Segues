62d8b5fc703d6539c4e7c2e80233d80e0a0bd006
jolie <joliesoc@gmail.com>
2022-07-19 11:16:36 -0400

    Initial Commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Jolie Charles on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Segues/end.swift
@@ -0,0 +1,29 @@
+//
+//  end.swift
+//  Segues
+//
+//  Created by Jolie Charles on 7/15/22.
+//
+
+import UIKit
+
+class end: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
